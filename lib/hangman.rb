a9f4d6808fc374b62a7d01565b7019867c02c5f3
DeweQ <thedeweq@yandex.ru>
2025-09-08 17:45:57 +0300

    Add game mechanics to Hangman class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,59 +3,77 @@ require "colorize"
 module Hangman
   # Contains state of hangman game
   class Hangman
-    @@PATH_TO_DICTIONARY_SOURCE = File.expand_path("../../sources/10000_english_words.txt", __FILE__)
-    
+    @@PATH_TO_DICTIONARY_SOURCE = File.expand_path("../sources/10000_english_words.txt", __dir__)
+    @MAX_HEALTH = 6
     attr_reader :alphabet, :health
-    def initialize
-      @MAX_HEALTH = 6
-      @health = @MAX_HEALTH - 2
-      @word = Hangman.get_random_word
-      @alphabet = ("a".."z").reduce({}) do |result, letter| 
+
+    def initialize(word)
+      @health = @MAX_HEALTH
+      @alphabet = ("A".."Z").each_with_object({}) do |letter, result|
         result[letter] = false
-        result
       end
     end
-    
-    def display_health
-      str = ""
-      health.times {str << "❤︎".colorize(:red)}
-      (@MAX_HEALTH-health).times { str << "❤︎".colorize(:white) }
-      puts str
+
+    def update_alphabet(letter)
+      @health -= 1 unless @word.include?(letter) || alphabet[letter]
+      alphabet[letter] = true 
     end
     
+    def finished?
+      health <= 0 || @word.chars.all? {|ch| @alphabet[ch]}
+    end
+    
+    def word
+      @word if finished?
+    end
+
+    def display_health
+      str = ""
+      health.times { str << "❤︎".colorize(:red) }
+      (@MAX_HEALTH - health).times { str << "❤︎".colorize(:white) }
+      puts str
+    end
+
     def display_word
       chars = @word.chars.map do |letter|
         if @alphabet[letter] then letter
-        else "_"
+        else
+          "_"
         end
       end
       puts chars.join(" ")
     end
-    
+
     def display_alphabet
       arr = @alphabet.map do |key, value|
-        if value && !word.include?(key) then
+        if value && !@word.include?(key)
           key.colorize(:red)
-        else if value && word.include?(key) then
+        elsif value && @word.include?(key)
           key.colorize(:green)
-        else key
+        else
+          key
         end
-      end
       end
       puts arr.join(" ")
     end
-    
+
+    def display
+      display_health
+      display_word
+      display_alphabet
+    end
+
     def self.load_dictionary
       f = File.open(@@PATH_TO_DICTIONARY_SOURCE)
       dictionary = []
-      while line = f.gets do
-        dictionary << line.chomp if line.chomp.size.between?(5,12)
+      while (line = f.gets)
+        dictionary << line.chomp if line.chomp.size.between?(5, 12)
       end
       dictionary
     end
-    
+
     def self.get_random_word
-      self.load_dictionary.sample
+      load_dictionary.sample
     end
   end
 end
